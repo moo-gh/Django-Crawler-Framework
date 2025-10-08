6281a68ed10e724101e27bbae49eefb7ab368f0a
Mohammad <mohammad.ghorbani@hotmail.com>
2025-10-08 11:45:33 +0330

    chore: update default HTTP headers in utils.py for improved compatibility and performance

--- crawler/agency/utils.py
+++ crawler/agency/utils.py
@@ -15,14 +15,16 @@ CODE = """
 IMAGE_FILE_TYPES: List[str] = ["jpeg", "jpg", "png", "bmp"]
 
 DEFAULT_HEADER = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
-    "AppleWebKit/537.11 (KHTML, like Gecko) "
-    "Chrome/23.0.1271.64 Safari/537.11",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-    "Accept-Encoding": "none",
-    "Accept-Language": "en-US,en;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/118.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
     "Connection": "keep-alive",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Cache-Control": "max-age=0",
 }
 
 
