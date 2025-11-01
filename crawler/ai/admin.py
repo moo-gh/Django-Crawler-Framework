a022951aa2466ea62d9a5a661a431ae9af186389
Mohammad <mohammad.ghorbani@hotmail.com>
2025-11-01 22:03:25 +0330

    wip(ai): register LLMModel

--- crawler/ai/admin.py
+++ crawler/ai/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
+from ai.models import LLMModel
+
+
+@admin.register(LLMModel)
+class LLMModelAdmin(ReadOnlyAdminDateFieldsMIXIN):
+    list_display = ("name", "provider", "input_token_price", "output_token_price")
+    list_filter = ("provider",)
+    search_fields = ("name",)
